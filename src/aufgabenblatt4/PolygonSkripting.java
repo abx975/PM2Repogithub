dad806d0d3ee031b6384af0bd8f8b8d556e53f4f
abx975 <nilseggeotmail.de>
2016-01-09 16:34:03 +0100

    Hinzufügen Blatt4

---
+++ src/aufgabenblatt4/PolygonSkripting.java
@@ -0,0 +1,76 @@
+/**
+ * Praktikum PM2, WS 15/16
+ *
+ * Aufgabe 4
+ */
+
+package aufgabenblatt4;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Klasse zur Darstellung der erlaubten Befehle
+ * 
+ * @author Nils Egge, Eduard Veit
+ *
+ */
+public class PolygonSkripting {
+	private final Pattern pattern;
+	private Matcher matcher;
+
+	/**
+	 * Konstruktor
+	 */
+	public PolygonSkripting() {
+		pattern = Pattern.compile("(bewege)\\s->" + "\\s(0|-?[0-9]*\\.?[0-9]*),\\s?(0|-?[0-9]*\\.?[0-9]*)");
+		matcher = null;
+	}
+
+	/**
+	 * Methode zum ueberpruefen, ob der eingebene Befehl ein gueltiger ist
+	 * 
+	 * @param befehl
+	 *            Der eingegebene Befehl
+	 * @return Gibt die Gueltigkeit des Befehls zurueck (true oder false)
+	 */
+	public boolean istGueltigerBefehl(String befehl) {
+		matcher = pattern.matcher(befehl);
+		return matcher.matches();
+	}
+
+	/**
+	 * Methode, um aus dem Eingabe String die X-Koordinate zu extrahieren
+	 * 
+	 * @param befehl
+	 *            Eingabe-String
+	 * @return X-Koordinate aus dem Eingabe-String
+	 * @throws Exception
+	 *             Bei falschen Befehl wird eine Exception geworfen
+	 */
+	public double getX(String befehl) throws Exception {
+		if (istGueltigerBefehl(befehl)) {
+			return new Double(matcher.group(3));
+		} else {
+			throw new Exception("Kein gueltiger Befehl!");
+		}
+	}
+
+	/**
+	 * Methode, um aus dem Eingabe String die X-Koordinate zu extrahieren
+	 * 
+	 * @param befehl
+	 *            Eingabe-String
+	 * @return Y-Koordinate aus dem Eingabe-String
+	 * @throws Exception
+	 *             Bei falschen Befehl wird eine Exception geworfen
+	 */
+	public double getY(String befehl) throws Exception {
+		if (istGueltigerBefehl(befehl)) {
+			return new Double(matcher.group(2));
+		} else {
+			throw new Exception("Kein gueltiger Befehl!");
+		}
+	}
+
+}
